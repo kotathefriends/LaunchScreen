3a6a86cd74cffa748232dc0f573aafd4a81e1305
kotathefriends <5070kouta@gmail.com>
2024-05-14 17:12:32 +0900

    Initial Commit

---
+++ LaunchScreen/LaunchScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  LaunchScreenApp.swift
+//  LaunchScreen
+//
+//  Created by KOTA TAKAHASHI on 2024/05/14.
+//
+
+import SwiftUI
+
+@main
+struct LaunchScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
